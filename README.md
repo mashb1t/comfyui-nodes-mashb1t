4966607f4805c784e6477fa6d30a14a341aa8e9d
Manuel Schmid <manuel.schmid@odt.net>
2024-06-11 17:55:37 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# ComfyUI mashb1t nodes
+Some custom nodes which make working with ComfyUI easier.
+
+## Description
+This Python script is an optional add-on to the Comfy UI stable diffusion client.
+
+## Getting Started
+Import into the custom nodes directory of your Comfy UI client.
+
+## Dependencies
+ComfyUI (obviously)
+
+## License
+This project is licensed under the MIT License.
---
+++ __init__.py
@@ -0,0 +1,98 @@
+import hashlib
+import os
+
+import numpy as np
+import torch
+from PIL import ImageSequence, ImageOps, Image
+
+import folder_paths
+import node_helpers
+
+
+class LoadImageCustom:
+    @classmethod
+    def INPUT_TYPES(s):
+        input_dir = folder_paths.get_input_directory()
+        files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
+        return {
+            "required": {
+                "image": (sorted(files), {"image_upload": True}),
+                "convert_to": (["RGB", "RGBA"],)
+            },
+        }
+
+    CATEGORY = "mashb1t"
+
+    RETURN_TYPES = ("IMAGE", "MASK")
+    FUNCTION = "load_image"
+
+    def load_image(self, image, convert_to):
+        image_path = folder_paths.get_annotated_filepath(image)
+
+        img = node_helpers.pillow(Image.open, image_path)
+
+        output_images = []
+        output_masks = []
+        w, h = None, None
+
+        excluded_formats = ['MPO']
+
+        for i in ImageSequence.Iterator(img):
+            i = node_helpers.pillow(ImageOps.exif_transpose, i)
+
+            if i.mode == 'I':
+                i = i.point(lambda i: i * (1 / 255))
+            image = i.convert(convert_to)
+
+            if len(output_images) == 0:
+                w = image.size[0]
+                h = image.size[1]
+
+            if image.size[0] != w or image.size[1] != h:
+                continue
+
+            image = np.array(image).astype(np.float32) / 255.0
+            image = torch.from_numpy(image)[None,]
+            if 'A' in i.getbands():
+                mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
+                mask = 1. - torch.from_numpy(mask)
+            else:
+                mask = torch.zeros((64, 64), dtype=torch.float32, device="cpu")
+            output_images.append(image)
+            output_masks.append(mask.unsqueeze(0))
+
+        if len(output_images) > 1 and img.format not in excluded_formats:
+            output_image = torch.cat(output_images, dim=0)
+            output_mask = torch.cat(output_masks, dim=0)
+        else:
+            output_image = output_images[0]
+            output_mask = output_masks[0]
+
+        return (output_image, output_mask)
+
+    @classmethod
+    def IS_CHANGED(s, image):
+        image_path = folder_paths.get_annotated_filepath(image)
+        m = hashlib.sha256()
+        with open(image_path, 'rb') as f:
+            m.update(f.read())
+        return m.digest().hex()
+
+    @classmethod
+    def VALIDATE_INPUTS(s, image):
+        if not folder_paths.exists_annotated_filepath(image):
+            return "Invalid image file: {}".format(image)
+
+        return True
+
+
+# A dictionary that contains all nodes you want to export with their names
+# NOTE: names should be globally unique
+NODE_CLASS_MAPPINGS = {
+    "mashb1t: LoadImage": LoadImageCustom
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "mashb1t: LoadImage": "Load Image (mashb1t)"
+}
